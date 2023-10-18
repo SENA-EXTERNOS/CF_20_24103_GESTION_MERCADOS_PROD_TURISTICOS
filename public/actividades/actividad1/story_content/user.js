1ee9377a86ead11ddf73d733a345b26b6cf7565a
Camilo Bolaño <80546125+elrilacho@users.noreply.github.com>
2023-10-18 00:10:03 -0500

    Componente_ajustado

--- public/actividades/actividad1/story_content/user.js
+++ public/actividades/actividad1/story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5sqVh3RQjeA":
+      case "6SPEBAhcs2D":
         Script1();
         break;
   }
--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,7 +7,7 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/opcion-multiple.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/ad.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo.color-primario
           h2 Cierre de eventos
@@ -18,7 +18,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Selección múltiple
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad1/index.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/actividad1/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
